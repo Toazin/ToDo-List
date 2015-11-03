5ba2c68823e1e71ddc435a8f5d8851f1a2355d7c
boaloca <toatzin_2@hotmail.com>
2015-11-02 20:55:46 -0600

    Initial Commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('toDoAppApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
